03d267abdfc87843e78a039ca968144c9fac180f
Joshua Glover <joshua.glover@klarna.com>
2017-03-16 09:42:40 +0100

    Remove specs

--- src/aws_dojo/drawing.clj
+++ src/aws_dojo/drawing.clj
@@ -1,25 +1,8 @@
 (ns aws-dojo.drawing
-  (:require [hiccup.core :as hiccup]
-            [clojure.spec :as s]))
+  (:require [hiccup.core :as hiccup]))
 
 (defn concatv [& xs]
   (->> (apply concat xs)
        (into [])))
-
-(s/def :command/command #{"createDrawing"
-                          "drawRectangle"
-                          "drawCircle"})
-
-(s/def :drawing/command (s/keys :req-un [:command/command]))
-
-(s/def :drawing/name string?)
-(s/def :drawing/html string?)
-
-(s/def :drawing/drawing (s/keys :req [:drawing/name
-                                      :drawing/html]))
-
-(s/fdef render
-        :args (s/cat :commands (s/coll-of :drawing/command))
-        :ret :drawing/drawing)
 
 (defn render [commands])
--- src/aws_dojo/sqs.clj
+++ src/aws_dojo/sqs.clj
@@ -1,35 +1,9 @@
 (ns aws-dojo.sqs
   (:require [amazonica.aws.sqs :as sqs]
-            [cheshire.core :as json]
-            [clojure.spec :as s]))
-
-(s/def :sqs/name string?)
-(s/def :sqs/url string?)
-
-(s/def :sqs/queue (s/keys :req [:sqs/name
-                                :sqs/url]))
-
-(s/def :sqs/message-id string?)
-(s/def :sqs/message-body any?)
-
-(s/def :sqs/message (s/keys :req [:sqs/message-id]
-                            :opt [:sqs/message-body]))
-
-(s/fdef create-queue!
-        :args (s/cat :name :sqs/queue-name)
-        :ret :sqs/queue)
+            [cheshire.core :as json]))
 
 (defn create-queue! [name])
 
-(s/fdef send-message
-        :args (s/cat :queue :sqs/queue
-                     :msg string?)
-        :ret :sqs/message)
-
 (defn send-message [queue msg])
-
-(s/fdef receive-message
-        :args (s/cat :queue :sqs/queue)
-        :ret (s/nilable :sqs/message))
 
 (defn receive-message [queue])
